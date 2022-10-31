8004e793e0fac0c0953de35813076f7a81737123
DiegoHernandezTL <analista.tecnologia3@grupocarrera.com.co>
2022-10-30 19:52:26 -0500

    State: Informe Tecnico - actions definidas

---
+++ src/app/state/actions/informe-tecnico.actions.ts
@@ -0,0 +1,34 @@
+import { createAction, props } from "@ngrx/store";
+import {InformeTecnico} from "../../models/informe-tecnico";
+
+// Acciones de carga
+
+export const loadInformes = createAction(
+  '[Informes List] Load informes'
+);
+
+export const loadedInformes = createAction(
+  '[Informes List] Loaded informes',
+  props<{informes: InformeTecnico[]}>()
+);
+
+// Creación de informe
+
+export const createInforme = createAction(
+  '[Informes List] Created informe',
+  props<{informe: InformeTecnico}>()
+);
+
+// Actualización de informe
+
+export const updateInforme = createAction(
+  '[Informes List] Updated informe',
+  props<{informe: InformeTecnico, id: number}>()
+);
+
+// Eliminación de informe
+
+export const deleteInforme = createAction(
+  '[Informes List] Delete informe',
+  props<{id: number}>()
+);
